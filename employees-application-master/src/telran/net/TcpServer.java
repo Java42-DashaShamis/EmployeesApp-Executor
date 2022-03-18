39e523ec1adcbab29e0c6582780a2238f246bfe4
korsa <korsa@Dellchik>
2022-03-18 21:53:36 +0200

    commits Executor to TcpServer

--- employees-application-master/src/telran/net/TcpServer.java
+++ employees-application-master/src/telran/net/TcpServer.java
@@ -1,16 +1,20 @@
 package telran.net;
 import java.io.IOException;
 import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 public class TcpServer implements Runnable {
-private int port;
-private ApplProtocol protocol;
-private ServerSocket serverSocket;
-public TcpServer(int port, ApplProtocol protocol) throws Exception{
-	this.port = port;
-	this.protocol = protocol;
-	serverSocket = new ServerSocket(port);
-	
-}
+	private int port;
+	private ApplProtocol protocol;
+	private ServerSocket serverSocket;
+	private ExecutorService executor;
+	private int N_THREADS = 3;
+	public TcpServer(int port, ApplProtocol protocol) throws Exception{
+		this.port = port;
+		this.protocol = protocol;
+		serverSocket = new ServerSocket(port);
+		executor = Executors.newFixedThreadPool(N_THREADS);
+	}
 	@Override
 	public void run() {
 		System.out.println("Server is listening on the port " + port);
@@ -18,15 +22,13 @@ public TcpServer(int port, ApplProtocol protocol) throws Exception{
 			try {
 				Socket socket = serverSocket.accept();
 				TcpClientServer client = new TcpClientServer(socket, protocol);
-				Thread threadClient = new Thread(client);
-				threadClient.start();
+				executor.execute(client);
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 				break;
 			}
 		}
-
+		executor.shutdown();
 	}
 
 }
